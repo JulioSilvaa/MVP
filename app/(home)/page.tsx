42aa67bf0b7b4e0fb8592c5efcc6d9370f87b7cd
JulioSilvaa <juliocesar.amancio@yahoo.com.br>
2025-11-15 11:39:20 -0300

    feat: adicionado a mensagem de bemvindo.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,22 @@
+import { ptBR } from "date-fns/locale";
 import Header from "../components/Header";
+import { format } from "date-fns"
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col gap-4 p-4 ">
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+        <p className="text-sm">{format(new Date(), "EEEE',' dd' de 'MMMM' ", {
+          locale: ptBR,
+        })}</p>
+      </div>
+
+
+
+
     </div>
   );
 }
